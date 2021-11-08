c921b9766f0712b97d4375b1661597056ede7450
Marcelo <mlizama@fidens.cl>
2021-11-08 14:44:12 -0300

    actualizacion de ramas

--- demo-git-submodule/src/app/app.module.ts
+++ demo-git-submodule/src/app/app.module.ts
@@ -4,13 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AutenticacionModule } from 'src/autenticacion/autenticacion.module';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AutenticacionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
